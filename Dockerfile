cd08a7caf2824030e5769d6701b47d7938e2ade7
Sven Boor <sven.boor@gmail.com>
2024-02-20 21:44:17 +0100

    Made code working with new api.

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
-FROM python:3.7-slim
+FROM python:3.9-slim
 
 RUN apt-get clean \
     && apt-get -y update
 RUN apt-get -y install python3-dev \
-    && apt-get -y install build-essential \ 
+    && apt-get -y install build-essential  
   
 COPY requirements.txt /
 RUN pip install -r /requirements.txt
--- main.py
+++ main.py
@@ -1,17 +1,36 @@
 import os
-from flask import Flask, request, jsonify
-import psycopg2
+from flask import Flask, request, jsonify, g
+from psycopg2.pool import SimpleConnectionPool
 import daf_stats
 
 app = Flask(__name__)
 
-conn_str = "dbname='daf'"
-if "IP" in os.environ:
-    conn_str += " host={} ".format(os.environ['IP'])
-if "DB_PASSWORD" in os.environ:
-    conn_str += " user=daf password={}".format(os.environ['DB_PASSWORD'])
+conn_str = f"dbname={os.getenv('DB_NAME')}"
 
-conn = psycopg2.connect(conn_str)
+if "DB_HOST" in os.environ:
+    conn_str += " host={} ".format(os.environ['DB_HOST'])
+if "DB_USER" in os.environ:
+    conn_str += " user={}".format(os.environ['DB_USER'])
+if "DB_PASSWORD" in os.environ:
+    conn_str += " password={}".format(os.environ['DB_PASSWORD'])
+if "DB_PORT" in os.environ:
+    conn_str += " port={}".format(os.environ['DB_PORT'])
+
+pgpool = SimpleConnectionPool(minconn=1, 
+        maxconn=5, 
+        dsn=conn_str)
+
+def get_db():
+    if 'db' not in g:
+        g.db = pgpool.getconn()
+    return g.db
+
+@app.teardown_appcontext
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        pgpool.putconn(db)
 
 class InvalidUsage(Exception):
     status_code = 400
@@ -34,13 +53,12 @@ def handle_invalid_usage(error):
     response.status_code = error.status_code
     return response
 
-
 @app.route('/stats/bikes/overview', methods=['GET'])
 def handle_bike_stats():
     gm_code = request.args.get('gm_code')
     if not gm_code:
         raise InvalidUsage("No gm_code specified.", status_code=400)
-    results = daf_stats.get_number_of_bikes_registered_in_total(conn, gm_code)
+    results = daf_stats.get_number_of_bikes_registered_in_total(get_db(), gm_code)
     return jsonify(results)
 
 @app.route('/stats/events/overview', methods=['GET'])
@@ -48,7 +66,7 @@ def handle_event_stats():
     gm_code = request.args.get('gm_code')
     if not gm_code:
         raise InvalidUsage("No gm_code specified.", status_code=400)
-    results = daf_stats.get_number_of_bikes_in_depot(conn, gm_code)
+    results = daf_stats.get_number_of_bikes_in_depot(get_db(), gm_code)
     return jsonify(results)
 
 @app.route('/stats/events/checkx', methods=['GET'])
@@ -56,7 +74,7 @@ def handle_event_stats_checkx():
     gm_code = request.args.get('gm_code')
     if not gm_code:
         raise InvalidUsage("No gm_code specified.", status_code=400)
-    results = daf_stats.get_stats_checkx(conn, gm_code)
+    results = daf_stats.get_stats_checkx(get_db(), gm_code)
     return jsonify(results)
 
 if __name__ == '__main__':
